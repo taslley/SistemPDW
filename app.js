f724a1fe7d76d5d5190f8a3d79f8544a47fc06a9
Tallis Tomé <tallitome2019@gmail.com>
2026-04-19 19:55:48 -0300

    Versão Final

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (botaoEnviar) {
         botaoEnviar.addEventListener('click', () => {
-            // CAPTURA (Verifique se esses seletores existem no seu HTML)
+
             const inputNome = document.querySelector('input[type="text"]');
             const inputEmail = document.querySelector('input[type="email"]');
-
-            // EXTRAÇÃO DOS VALORES
             const valorNome = inputNome.value;
             const valorEmail = inputEmail.value;
 
-            // VALIDAÇÃO
             if (valorNome === '' || valorEmail === '') {
                 alert('Por favor, preencha o Nome e o E-mail.');
                 return;
             }
             
-            // REDIRECIONAMENTO
             window.location.href = "concluido.html";
         });
     }
